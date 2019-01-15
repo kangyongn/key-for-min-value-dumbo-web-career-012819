ed2a941a114e6a8a4e43067b5d8f8c097243041d
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-15 03:59:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,11 @@
 def key_for_min_value(name_hash)
   min_key = ""
   min = 999999999
+  
+  if name_hash == {}
+    return nil
   name_hash.each do |key, value|
-    if name_hash == {}
-      return nil
+
     else
       if value < min
         min = value
